b1cb37c5810f8b0de6533122ef6fc05c0103f00a
Alex Wilhelm <awilh375@gmail.com>
2026-03-10 15:01:47 +0000

    update server url to rand0m

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://rand0m.tplinkdns.com/arcade';
 export const TOKEN_KEY = 'arcade-token';
 export const THEME_KEY = 'arcade-theme';
